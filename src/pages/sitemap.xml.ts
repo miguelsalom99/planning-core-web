152e698ac3ec91943701e7d85dba32d851d03ce0
Miguel Salom Jimenez <miguel.salom@student.ie.edu>
2026-09-03 19:05:12 +0200

    Reposition homepage around measured robustness + add /comparativa page

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -5,23 +5,15 @@ const SITE = 'https://planning-core-web.vercel.app';
 
 export const prerender = true;
 
-const staticPaths = ['', 'producto', 'precios', 'contacto', 'sectores'];
+const staticPaths = ['', 'producto', 'comparativa', 'precios', 'contacto', 'sectores'];
 
 export const GET: APIRoute = () => {
   const urls = [
-    ...staticPaths.map((p) => `${SITE}/${p}`),
+    ...staticPaths.map(p => `${SITE}/${p}`),
     ...SECTORS.map((s: { slug: string }) => `${SITE}/sectores/${s.slug}`),
   ];
-
-  const body = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls
-  .map((loc) => `  <url><loc>${loc}</loc><changefreq>monthly</changefreq></url>`)
-  .join('\n')}
-</urlset>
-`;
-
-  return new Response(body, {
-    headers: { 'Content-Type': 'application/xml; charset=utf-8' },
-  });
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls
+    .map(loc => `  <url><loc>${loc}</loc><changefreq>monthly</changefreq></url>`)
+    .join('\n')}\n</urlset>\n`;
+  return new Response(body, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
 };
